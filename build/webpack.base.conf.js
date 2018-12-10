df22e85748af203dd3242b019759f3850492ad8d
Sébastien Champoux <sebs.champoux@gmail.com>
2018-12-09 23:59:15 -0500

    Ajouté les fichiers SCSS et configuré pour la compilation

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,6 +74,25 @@ module.exports = {
 					limit: 10000,
 					name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
 				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'vue-style-loader',
+					{
+						loader: 'css-loader',
+						options: {importLoaders: 1}
+					},
+					'postcss-loader'
+				]
 			}
 		]
 	},
